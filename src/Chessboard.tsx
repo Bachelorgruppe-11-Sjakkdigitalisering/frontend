f405e9c5509010ee9b60244ef511568dec505987
Herman Lundby-Holen <hermanluho@gmail.com>
2026-02-05 13:42:35 +0100

    feat: resize board, add example position, allow drawing arrows

--- src/Chessboard.tsx
+++ src/Chessboard.tsx
@@ -2,7 +2,7 @@ import { Chessboard } from "react-chessboard";
 
 export default function ChessboardView() {
   const options = {
-    // denne posisjonen er hentet fra et random parti jeg har spilt
+    // denne posisjonen er hentet fra et random parti jeg har spilt (eksempel på en FEN streng)
     position: "rn3rk1/4Qpp1/p1p4p/2p1p3/2P3b1/3P1NP1/PP2PPBP/R4RK1 b - - 0 14",
     // finnes utrolig mange "options" (sjekk docs for å se alle)
     // denne under tillater å tegne piler feks
