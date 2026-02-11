3fca7bb7e8dbd66625a58bd0af59c172a374a2ee
Herman Lundby-Holen <hermanluho@gmail.com>
2026-02-11 14:40:04 +0100

    style: game view

--- src/components/chessboards/GameView.stories.tsx
+++ src/components/chessboards/GameView.stories.tsx
@@ -11,7 +11,18 @@ const meta = {
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const Default: Story = {
+export const Pending: Story = {
+  args: {
+    fen: "rn3rk1/4Qpp1/p1p4p/2p1p3/2P3b1/3P1NP1/PP2PPBP/R4RK1 b - - 0 14",
+    whitePlayerName: "Dennis Johansen",
+    whitePlayerTime: "00:22",
+    blackPlayerName: "Herman Lundby-Holen",
+    blackPlayerTime: "00:14",
+    status: "PENDING",
+  },
+};
+
+export const WhiteToMove: Story = {
   args: {
     fen: "rn3rk1/4Qpp1/p1p4p/2p1p3/2P3b1/3P1NP1/PP2PPBP/R4RK1 b - - 0 14",
     whitePlayerName: "Dennis Johansen",
@@ -19,5 +30,16 @@ export const Default: Story = {
     blackPlayerName: "Herman Lundby-Holen",
     blackPlayerTime: "00:14",
     status: "WHITE_TO_MOVE",
+  },
+};
+
+export const BlackToMove: Story = {
+  args: {
+    fen: "rn3rk1/4Qpp1/p1p4p/2p1p3/2P3b1/3P1NP1/PP2PPBP/R4RK1 b - - 0 14",
+    whitePlayerName: "Dennis Johansen",
+    whitePlayerTime: "00:22",
+    blackPlayerName: "Herman Lundby-Holen",
+    blackPlayerTime: "00:14",
+    status: "BLACK_TO_MOVE",
   },
 };
--- src/components/chessboards/GameView.tsx
+++ src/components/chessboards/GameView.tsx
@@ -1,5 +1,6 @@
 import { Avatar, Slider, Typography } from "@mui/material";
 import { Chessboard, type ChessboardOptions } from "react-chessboard";
+import "./GameView.css";
 
 /* TODO: man må kanskje hente avatar her hvis vi skal ha med det? */
 type GameViewProps = {
@@ -24,34 +25,41 @@ export default function GameView({
   };
 
   return (
-    <div>
-      <div>
-        <div>
-          <div>
-            <Avatar />
-            <Typography>{blackPlayerName}</Typography>
-          </div>
-          <Typography>{blackPlayerTime}</Typography>
+    <div className="game-view">
+      <div className="player-info">
+        <div className="flex-row">
+          <Avatar />
+          <Typography variant="subtitle2">{blackPlayerName}</Typography>
         </div>
+        <Typography
+          className={"time " + (status === "BLACK_TO_MOVE" ? "active" : "")}
+        >
+          {blackPlayerTime}
+        </Typography>
+      </div>
 
-        <div>
-          <Slider
-            aria-label="Game Evaluation"
-            defaultValue={0}
-            min={-100}
-            max={100}
-            valueLabelDisplay="on"
-          />
-          <Chessboard options={options} />
-        </div>
+      <div className="board-eval">
+        <Slider /* TODO: lage gene eval bar komponent?? må hvertfall styles bedre */
+          aria-label="Game Evaluation"
+          orientation="vertical"
+          sx={{ height: "inherit" }}
+          value={50}
+          max={100}
+          disabled
+        />
+        <Chessboard options={options} />
+      </div>
 
-        <div>
-          <div>
-            <Avatar />
-            <Typography>{whitePlayerName}</Typography>
-          </div>
-          <Typography>{whitePlayerTime}</Typography>
+      <div className="player-info">
+        <div className="flex-row">
+          <Avatar />
+          <Typography variant="subtitle2">{whitePlayerName}</Typography>
         </div>
+        <Typography
+          className={"time " + (status === "WHITE_TO_MOVE" ? "active" : "")}
+        >
+          {whitePlayerTime}
+        </Typography>
       </div>
     </div>
   );
