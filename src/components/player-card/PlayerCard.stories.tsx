5e20eff5b97c237a52e721bac3b62546c7a4d1c6
Herman Lundby-Holen <hermanluho@gmail.com>
2026-02-19 17:11:40 +0100

    test: storybook tests for player card

---
+++ src/components/player-card/PlayerCard.stories.tsx
@@ -0,0 +1,17 @@
+import type { Meta, StoryObj } from "@storybook/react-vite";
+import PlayerCard from "./PlayerCard";
+
+const meta = {
+  component: PlayerCard,
+  title: "Player Card",
+  tags: ["autodocs"],
+} satisfies Meta<typeof PlayerCard>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    name: "Herman Lundby-Holen",
+  },
+};
