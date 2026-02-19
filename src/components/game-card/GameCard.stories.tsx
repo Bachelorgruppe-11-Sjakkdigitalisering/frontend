cb8c2a236f25d941e2b1cba1da8f96f45f84e9c0
Herman Lundby-Holen <hermanluho@gmail.com>
2026-02-19 17:09:55 +0100

    test: storybook tests for game card component

---
+++ src/components/game-card/GameCard.stories.tsx
@@ -0,0 +1,27 @@
+import type { Meta, StoryObj } from "@storybook/react-vite";
+import GameCard from "./GameCard";
+
+const meta = {
+  component: GameCard,
+  title: "Game Card",
+  tags: ["autodocs"],
+} satisfies Meta<typeof GameCard>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const WhitePlayerWin: Story = {
+  args: {
+    whiteName: "Herman Lundby-Holen",
+    blackName: "Dennis Johansen",
+    whiteWin: true,
+  },
+};
+
+export const BlackPlayerWin: Story = {
+  args: {
+    whiteName: "Herman Lundby-Holen",
+    blackName: "Dennis Johansen",
+    whiteWin: false,
+  },
+};
