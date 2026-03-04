f17708207959c49fa1456e99190d58207930ee61
Herman Lundby-Holen <hermanluho@gmail.com>
2026-03-04 11:45:30 +0100

    feat: create hook and call for players in the database

---
+++ src/api/players.ts
@@ -0,0 +1,15 @@
+export default async function fetchPlayers(playerName: string) {
+  const queryParam = playerName
+    ? `?player=${encodeURIComponent(playerName)}`
+    : "";
+
+  const response = await fetch(
+    `http://127.0.0.1:8000/api/players${queryParam}`,
+  );
+
+  if (!response.ok) {
+    throw new Error("Could not players."); // TODO: bedre håndtering her
+  }
+
+  return response.json();
+}
---
+++ src/hooks/usePlayers.ts
@@ -0,0 +1,11 @@
+import { useQuery } from "@tanstack/react-query";
+import fetchPlayers from "../api/players";
+
+export function usePlayers(playerName: string) {
+  return useQuery({
+    queryKey: ["players", playerName], // this makes it so we automatically refetch whenever playerName changes
+    queryFn: () => fetchPlayers(playerName),
+    staleTime: 1000 * 60, // after 1 min, react query refetches in the background to see if there are any new games
+    placeholderData: (previousData) => previousData, // keeps previous data while fetching new data to prevent UI flickering
+  });
+}
