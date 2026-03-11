2889371936c9b60e909f8be2cd6751501df95370
Herman Lundby-Holen <hermanluho@gmail.com>
2026-03-11 10:26:00 +0100

    docs: document the player card component

--- src/components/player-card/PlayerCard.tsx
+++ src/components/player-card/PlayerCard.tsx
@@ -2,16 +2,37 @@ import { Avatar, Typography, useTheme } from "@mui/material";
 import "./PlayerCard.css";
 import { useNavigate } from "react-router";
 
+/**
+ * Defines the properties for the {@link PlayerCard} component.
+ */
 type PlayerCardProps = {
   // TODO: legge til avatar her hvis vi skal beholde det
+  /** The display name of the player. */
   name: string;
+  /** The unique UUID or string identifier for the player in the database. */
   playerId: string;
 };
 
+/**
+ * A UI card component that displays the player name and avatar.
+ * * It renders the player name together with an avatar of the player if provided in the database.
+ * If clicked on, it navigates to the full player profile page.
+ *
+ * @example Usage of component:
+ * ```ts
+ * <PlayerCard
+ *  name="Magnus Carlsen"
+ *  playerId="12345"
+ * />
+ * ```
+ */
 export default function PlayerCard({ name, playerId }: PlayerCardProps) {
   const theme = useTheme();
   const navigate = useNavigate();
 
+  /**
+   * Triggers the navigation to the player's specific page.
+   */
   const handleClick = () => {
     navigate(`/player/${playerId}`);
   };
