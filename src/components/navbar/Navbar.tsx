4946c7c61c66a0aa6071ad5f744965bc367a18c3
Herman Lundby-Holen <hermanluho@gmail.com>
2026-03-11 10:38:59 +0100

    docs: document the navbar component

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -14,19 +14,38 @@ import {
 } from "@mui/material";
 import { Link, useLocation, useNavigate } from "react-router";
 
+/**
+ * The 'source of truth' for the application's primary navigation routes.
+ * Modifying this will automatically update both mobile and desktop routes.
+ */
 const NAV_ITEMS = [
   { label: "Hjem", icon: <HouseRounded />, path: "/" },
   { label: "Database", icon: <InboxRounded />, path: "/database" },
   { label: "Innstillinger", icon: <Settings />, path: "/settings" },
 ];
 
+/**
+ * A responsive global navigation component for the application.
+ * * On desktop (breakpoints: 'md' and up) it renders as a permament side drawer.
+ * * On mobile (breakpoints: 'md' and down) it renders as a fixed bottom navigation anchor.
+ *
+ * **Dependecies:**
+ * * Relies on 'react-router' context for tracking the active path and for navigation.
+ * * Relies on Material UI's 'ThemeProvider' for colors and breakpoints.
+ *
+ * @example Usage of component:
+ * ```ts
+ * <Navbar />
+ * ```
+ */
 export default function Navbar() {
   const theme = useTheme();
+  // Determines if the viewport is medium or larger
   const isDesktop = useMediaQuery(theme.breakpoints.up("md"));
   const location = useLocation();
   const navigate = useNavigate();
 
-  // determine the active state
+  // Determines the active state to highlight the correct nav item
   const currentPath = location.pathname;
 
   if (isDesktop) {
