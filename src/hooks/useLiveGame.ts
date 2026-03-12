108c00e51a4de0e1f947cd39908345774c5b4e91
Herman Lundby-Holen <hermanluho@gmail.com>
2026-03-12 14:43:47 +0100

    docs: document useLiveGame hook

--- src/hooks/useLiveGame.ts
+++ src/hooks/useLiveGame.ts
@@ -1,6 +1,18 @@
 import { useQuery } from "@tanstack/react-query";
 import fetchLiveGameData from "../api/game";
 
+/**
+ * A custom React Query hook that manages fetching and caching live chess games based on a `boardId`.
+ *
+ * **Caching and refetch strategy:**
+ * - **Query key:** Automatically triggers a refetch whenever `boardId` changes.
+ * - **Enabled:** Only runs if we have a `boardId` and if the `isLive` boolean is true.
+ * - **Polling:** Automatically refetch every second.
+ *
+ * @param boardId The UUID of the board/game to fetch.
+ * @param isLive A boolean to tell if the game to fetch is live or not. Must be true for this hook to trigger.
+ * @returns The standard React Query result object containing `data`, `isLoading`, `isError`, etc.
+ */
 export default function useLiveGame(
   boardId: string | undefined,
   isLive: boolean,
@@ -8,7 +20,7 @@ export default function useLiveGame(
   return useQuery({
     queryKey: ["liveGame", boardId],
     queryFn: () => fetchLiveGameData(boardId),
-    enabled: isLive && !!boardId, // only run this hook if it's a live game and we have an id
-    refetchInterval: 1000, // auto refetch every second
+    enabled: isLive && !!boardId,
+    refetchInterval: 1000,
   });
 }
