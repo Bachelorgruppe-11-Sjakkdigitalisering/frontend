e59a8a42c744f6955eb7d8c62e44efdbd1677e88
Herman Lundby-Holen <hermanluho@gmail.com>
2026-03-12 14:18:54 +0100

    docs: document useGame hook

--- src/hooks/useGame.ts
+++ src/hooks/useGame.ts
@@ -1,11 +1,23 @@
 import { useQuery } from "@tanstack/react-query";
 import { fetchArchivedGameDetails } from "../api/database";
 
+/**
+ * A custom React Query hook that manages fetching and caching a specific archived game based on the unique `gameId`.
+ *
+ * **Caching and refetch strategy:**
+ * - **Query key:** Automatically refetches in background whenever the `gameId` changes.
+ * - **Enabled:** We only run this hook if it is an archived game to display, and if we have a game id.
+ * - **Stale time:** Since its an archived game, it never changes, so we cache for `Infinity`.
+ *
+ * @param gameId The UUID of the game to fetch.
+ * @param isLive A boolean to tell if we are going to dispay a live game or not.
+ * @returns The standard React Query result object containing `data`, `isLoading`, `isError`, etc.
+ */
 export default function useGame(gameId: string | undefined, isLive: boolean) {
   return useQuery({
     queryKey: ["game", gameId],
     queryFn: () => fetchArchivedGameDetails(gameId),
-    enabled: !isLive && !!gameId, // only run hook if game is not live and we have a game id
-    staleTime: Infinity, // archived games don't change so we can cache for a long time
+    enabled: !isLive && !!gameId,
+    staleTime: Infinity,
   });
 }
