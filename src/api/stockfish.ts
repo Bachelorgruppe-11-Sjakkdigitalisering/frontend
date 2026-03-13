0caa04d04e6ba6eabeed2a3abdcedaea8a9227d2
Herman Lundby-Holen <hermanluho@gmail.com>
2026-03-13 10:27:00 +0100

    docs: document the stockfish api request

--- src/api/stockfish.ts
+++ src/api/stockfish.ts
@@ -1,3 +1,10 @@
+/**
+ * Posts a new request to chess/stockfish api to retrieve the evaluation of a chess position given by the `fen`.
+ *
+ * @param fen The standard Forsynth-Edwards Notation string representing the current board state.
+ * @returns A promise returning the stockfish data of the current position.
+ * @throws {Error} Throws an error if the stockfish api returns a non-2xx status code.
+ */
 export default async function fetchEvaluation(fen: string) {
   const response = await fetch("https://chess-api.com/v1", {
     method: "POST",
@@ -6,7 +13,7 @@ export default async function fetchEvaluation(fen: string) {
   });
 
   if (!response.ok) {
-    throw new Error("Network response was not ok"); // TODO: legg til bedre handling av error
+    throw new Error(`Stockfish response was not ok: ${response.statusText}`);
   }
 
   return response.json();
